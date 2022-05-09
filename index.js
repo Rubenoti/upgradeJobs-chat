c5b581d2b3851733f15f69badf22dd2b390d2003
Ruben <ruben.infantes@gmail.com>
2022-05-09 12:11:33 +0200

    fix

--- index.js
+++ index.js
@@ -1,48 +1,8 @@
 const express = require('express');
-const cors = require('cors');
-const { connect } = require('./src/utils/db/db');
-const chats = require('./src/api/chat/chat.routes');
 const http = require('http');
 const { Server } = require('socket.io');
 
-connect()
 const app = express();
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-app.use(
-    cors({
-        origin: [
-            'http://localhost:3000',
-            'https://localhost:4200',
-            'https://localhost:5500',
-            'http://127.0.0.1:5500',
-            'https://upgrade-jobs-app.vercel.app',
-        ],
-        credentials: true,
-    })
-);
-
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ extended: true, limit: '5mb' }));
-
-app.use('/chats', chats);
-
-app.use((req, res, next) => {
-    let err = new Error();
-    err.status = 404;
-    err.message = HTTPSTATUSCODE[404];
-    next(err);
-});
-
-app.use((err, req, res, next) => {
-    return res.status(err.status || 500).json(err.message || 'Unexpected error');
-});
-app.disable('x-powered-by');
 
 const serverM = http.createServer(app);
 const io = new Server(serverM, {
